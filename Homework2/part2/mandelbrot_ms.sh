6af95eea4ec01c6c46424f0f68ae39a0210d6297
Jerry <jerrl10@uci.edu>
2019-05-12 16:28:59 -0700

    changed to ms_sh to 64

--- Homework2/part2/mandelbrot_ms.sh
+++ Homework2/part2/mandelbrot_ms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#$ -N Mandelbrot
-#$ -q class
+#$ -N Mandelbrot_MS
+#$ -q pub8i
 #$ -pe mpi 64
 #$ -R y
 #$ -ckpt restart
@@ -18,4 +18,4 @@ module load boost/1.57.0
 module load openmpi-1.8.3/gcc-4.9.2
 
 # Run the program 
-mpirun -np 16  ./mandelbrot_ms 1000 1000
+mpirun -np 64  ./mandelbrot_ms 1000 1000
