8284926ab5ddea09486eee0b731c9063903f9533
Jerry <jerrl10@uci.edu>
2019-05-12 19:46:46 -0700

    asdf

--- Homework2/part2/mandelbrot_susie.cc
+++ Homework2/part2/mandelbrot_susie.cc
@@ -6,11 +6,17 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <mpi.h>
 
 int
 main (int argc, char* argv[])
 {
   /* Lucky you, you get to write MPI code */
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,	/* always use this */
+			&myrank);	/* process rank, 0 thru N-1 */
+    MPI_Comm_size(MPI_COMM_WORLD,	/* always use this */
+			&ntasks);	/* #processes in application */
 }
 
 /* eof */
