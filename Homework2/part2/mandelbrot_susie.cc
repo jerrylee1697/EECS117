8c5dbf30e12cbc9b7caf761a1900c3775aa211e2
Jerry <jerrl10@uci.edu>
2019-05-12 19:48:56 -0700

    remove susie code

--- Homework2/part2/mandelbrot_susie.cc
+++ Homework2/part2/mandelbrot_susie.cc
@@ -12,11 +12,12 @@ int
 main (int argc, char* argv[])
 {
   /* Lucky you, you get to write MPI code */
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD,	/* always use this */
-			&myrank);	/* process rank, 0 thru N-1 */
-    MPI_Comm_size(MPI_COMM_WORLD,	/* always use this */
-			&ntasks);	/* #processes in application */
+    // MPI_Init(&argc, &argv);
+    // MPI_Comm_rank(MPI_COMM_WORLD,	/* always use this */
+	// 		&myrank);	/* process rank, 0 thru N-1 */
+    // MPI_Comm_size(MPI_COMM_WORLD,	/* always use this */
+	// 		&ntasks);	/* #processes in application */
+    
 }
 
 /* eof */
