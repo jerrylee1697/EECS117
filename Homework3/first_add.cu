3d1f36526344f76bc9a1b19f5f27a90c5594ede8
Jerry <jerrl10@uci.edu>
2019-05-25 02:42:21 -0700

    test firstadd with normal

--- Homework3/first_add.cu
+++ Homework3/first_add.cu
@@ -62,22 +62,27 @@ dtype reduce_cpu(dtype *data, int n) {
 __global__ void
 kernel3(dtype *g_idata, dtype *g_odata, unsigned int n)
 {
-	__shared__  dtype scratch[MAX_THREADS/2];
+	__shared__  dtype scratch[MAX_THREADS];
 
     unsigned int bid = gridDim.x * blockIdx.y + blockIdx.x;
     unsigned int i = bid * blockDim.x + threadIdx.x;	// Global Thread ID
 
-	unsigned int half = MAX_THREADS/2;
-	// Cuts down threads used by half
-    if(i + half < n && threadIdx.x < half ) {
-        scratch[threadIdx.x] = g_idata[i] + g_idata[i + half]; 
-    } else if (threadIdx.x < half) {
+	// unsigned int half = MAX_THREADS/2;
+	// // Cuts down threads used by half
+    // if(i + half < n && threadIdx.x < half ) {
+    //     scratch[threadIdx.x] = g_idata[i] + g_idata[i + half]; 
+    // } else if (threadIdx.x < half) {
+    //     scratch[threadIdx.x] = 0;
+	// }
+	if(i < n) {
+        scratch[threadIdx.x] = g_idata[i]; 
+    } else {
         scratch[threadIdx.x] = 0;
     }
     __syncthreads ();
 
 	// One less stride 
-	for(unsigned int s = blockDim.x / 4; s > 0; s = s >> 1) {
+	for(unsigned int s = blockDim.x / 2; s > 0; s = s >> 1) {
         // Modify Here
         if (threadIdx.x < s) {
             scratch[threadIdx.x] += scratch[s + threadIdx.x];
